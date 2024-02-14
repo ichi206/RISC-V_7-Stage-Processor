249dfaeff206096e47c4c779eae8a5392bc7e219
Nicholas Altemeier <110011734+npalt@users.noreply.github.com>
2024-02-13 18:12:35 -0800

    Update memory.sv

--- memory/memory.sv
+++ memory/memory.sv
@@ -3,20 +3,21 @@
 
 module memory
 (
-	input  word_address addr,
+	input  word_address long_addr,
 	input  load_type how_much,
 	
 	output word content
 );
 
-	logic [] effective_address
+	wire [`memory_bits - 1 : 0] addr = long_addr[`memory_bits - 1 : 0];
+	wire [`memory_bits - 3 : 0] word_addr = addr[`memory_bits - 1 : 2];
 
-	reg [7 : 0] mem_b0 [0 : `memory_size / 4 - 1];
-	reg [7 : 0] mem_b1 [0 : `memory_size / 4 - 1];
-	reg [7 : 0] mem_b2 [0 : `memory_size / 4 - 1];
-	reg [7 : 0] mem_b3 [0 : `memory_size / 4 - 1];
+	byte mem_b0 [0 : `memory_size / 4 - 1];
+	byte mem_b1 [0 : `memory_size / 4 - 1];
+	byte mem_b2 [0 : `memory_size / 4 - 1];
+	byte mem_b3 [0 : `memory_size / 4 - 1];
 	
-	always_comb
+	always_comb 
 	begin
 		case (how_much)
 			
@@ -27,7 +28,7 @@ module memory
 				content = 0; // TODO
 			
 			WORD:
-				content = {mem_b0, mem_b1, mem_b2, mem_b3};
+				content = {mem_b0[word_addr], mem_b1[word_addr], mem_b2[word_addr], mem_b3[word_addr]};
 		
 		endcase
 	end
